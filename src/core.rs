72d5197f22122cfeb6a0b220147245fd99fffda9
xcshuan <xcshuan@hotmail.com>
2022-07-29 15:29:03 +0800

    rm rayon dependency

--- src/core.rs
+++ src/core.rs
@@ -2,7 +2,6 @@
 
 use std::borrow::{Borrow, Cow};
 
-use rayon::join;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 use crate::traits::*;
@@ -239,14 +238,14 @@ impl<'m, 'd> Encrypt<DecryptionKey, RawPlaintext<'m>, RawCiphertext<'d>> for Pai
         let dk_n = &dk.q * &dk.p;
         let dk_ppinv = BigInt::mod_inv(&dk_pp, &dk_qq).unwrap();
         let (mp, mq) = crt_decompose(m.0.borrow(), &dk_pp, &dk_qq);
-        let (cp, cq) = join(
-            || {
+        let (cp, cq) = (
+            {
                 let rp = BigInt::sample_below(&dk.p);
                 let rnp = BigInt::mod_pow(&rp, &dk_n, &dk_pp);
                 let gmp = (1 + mp * &dk_n) % &dk_pp; // TODO[Morten] maybe there's more to get here
                 (gmp * rnp) % &dk_pp
             },
-            || {
+            {
                 let rq = BigInt::sample_below(&dk.q);
                 let rnq = BigInt::mod_pow(&rq, &dk_n, &dk_qq);
                 let gmq = (1 + mq * &dk_n) % &dk_qq; // TODO[Morten] maybe there's more to get here
@@ -273,13 +272,13 @@ impl<'m, 'r, 'd>
         let dk_ppinv = BigInt::mod_inv(&dk_pp, &dk_qq).unwrap();
         let (mp, mq) = crt_decompose(m.0.borrow(), &dk_pp, &dk_qq);
         let (rp, rq) = crt_decompose(&r.0, &dk_pp, &dk_qq);
-        let (cp, cq) = join(
-            || {
+        let (cp, cq) = (
+            {
                 let rnp = BigInt::mod_pow(&rp, &dk_n, &dk_pp);
                 let gmp = (1 + mp * &dk_n) % &dk_pp; // TODO[Morten] maybe there's more to get here
                 (gmp * rnp) % &dk_pp
             },
-            || {
+            {
                 let rnq = BigInt::mod_pow(&rq, &dk_n, &dk_qq);
                 let gmq = (1 + mq * &dk_n) % &dk_qq; // TODO[Morten] maybe there's more to get here
                 (gmq * rnq) % &dk_qq
@@ -353,14 +352,14 @@ impl<'c, 'm> Decrypt<DecryptionKey, &'c RawCiphertext<'c>, RawPlaintext<'m>> for
         let dk_hq = h(&dk.q, &dk_qq, &dk_n);
         let (cp, cq) = crt_decompose(c.0.borrow(), &dk_pp, &dk_qq);
         // decrypt in parallel with respectively p and q
-        let (mp, mq) = join(
-            || {
+        let (mp, mq) = (
+            {
                 // process using p
                 let dp = BigInt::mod_pow(&cp, &dk_pminusone, &dk_pp);
                 let lp = l(&dp, &dk.p);
                 (&lp * &dk_hp) % &dk.p
             },
-            || {
+            {
                 // process using q
                 let dq = BigInt::mod_pow(&cq, &dk_qminusone, &dk_qq);
                 let lq = l(&dq, &dk.q);
